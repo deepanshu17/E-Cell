37a32f1a53e75532e17e91d61b2df7e0acc76e5b
Deepanshu Jain <dimps1796@gmail.com>
2016-08-03 23:23:06 +0530

    Footer file changed

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -6,77 +6,32 @@
                 ============================================= -->
                 <div class="footer-widgets-wrap clearfix">
 
-                    <div class="col_one_third">
+                    <div class="col_two_third col_last">
 
                         <div class="widget clearfix">
 
-                        
+                            <div>
+                                <h3> Contact us </h3>
+                            </div>
 
-                            <div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
-                                <address>
-                                    <i class="icon-location"></i>
-                                    <strong>Address:</strong><br>
-                                    IIIT Hyderabad<br>
-                                    Gachibowli 500032<br>
-                                </address>
-                                <i class="icon-phone3" style="margin: 5px"></i> <abbr title="Phone Number"><strong>Phone:</strong></abbr> +91 9581248002<br>
-                                <i class="icon-email3" style="margin: 5px"></i>  <abbr title="Email Address"><strong>Email:</strong></abbr> ecell@iiit.ac.in
+                            <div>
+
+                                <i class="icon-location" style="margin: 5px"></i> <abbr title="Address">
+                                    <strong>Address: </strong></abbr> CIE, Vindhya C4, IIIT Hyderabad, <br> Gachibowli, Hyderabad - 500032 <br><br>
+
+                                <i class="icon-email3" style="margin: 5px"></i>  <abbr title="Email Address"><strong>Email:</strong></abbr> ecell@iiit.ac.in <br>
+
+                                <i class="icon-phone3" style="margin: 5px"></i> <abbr title="Phone Number"><strong>Phone:</strong></abbr> +91-9581248002 <br>
+
                             </div>
 
                         </div>
 
                     </div>
 
-                    <div class="col_half">
-
-                        <div class="widget clearfix">
-                            <h4>Client Testimonials</h4>
-
-                            <div class="fslider testimonial no-image nobg noborder noshadow nopadding" data-animation="slide" data-arrows="false">
-                                <div class="flexslider">
-                                    <div class="slider-wrap">
-                                        <div class="slide">
-                                            <div class="testi-image">
-                                                <a href="#"><img src="images/testimonials/3.jpg" alt="Customer Testimonails"></a>
-                                            </div>
-                                            <div class="testi-content">
-                                                <p>Similique fugit repellendus expedita excepturi iure perferendis provident quia eaque. Repellendus, vero numquam?</p>
-                                                <div class="testi-meta">
-                                                    Steve Jobs
-                                                    <span>Apple Inc.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="slide">
-                                            <div class="testi-image">
-                                                <a href="#"><img src="images/testimonials/2.jpg" alt="Customer Testimonails"></a>
-                                            </div>
-                                            <div class="testi-content">
-                                                <p>Natus voluptatum enim quod necessitatibus quis expedita harum provident eos obcaecati id culpa corporis molestias.</p>
-                                                <div class="testi-meta">
-                                                    Collis Ta'eed
-                                                    <span>Envato Inc.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="slide">
-                                            <div class="testi-image">
-                                                <a href="#"><img src="images/testimonials/1.jpg" alt="Customer Testimonails"></a>
-                                            </div>
-                                            <div class="testi-content">
-                                                <p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
-                                                <div class="testi-meta">
-                                                    John Doe
-                                                    <span>XYZ Inc.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
+                    <div class="col_one_third topmargin-lg fright">
+                        <div class="widget clearfix"></div>
+                        <div class="widget clearfix"></div>
                         <div class="widget clearfix">
 
                             <a href="https://www.facebook.com/ecell.iiithyd/" class="social-icon si-small si-rounded si-facebook">
@@ -89,7 +44,7 @@
                                 <i class="icon-twitter"></i>
                             </a>
 
-                            <!-- <a href="#" class="social-icon si-small si-rounded si-gplus">
+                            <a href="#" class="social-icon si-small si-rounded si-gplus">
                                 <i class="icon-gplus"></i>
                                 <i class="icon-gplus"></i>
                             </a>
@@ -97,7 +52,7 @@
                             <a href="#" class="social-icon si-small si-rounded si-pinterest">
                                 <i class="icon-youtube"></i>
                                 <i class="icon-youtube"></i>
-                            </a> -->
+                            </a>
 
                         </div>
 
